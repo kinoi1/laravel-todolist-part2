99353f9adfe4889107c81b1f12e47bb4a8418258
kinoi1 <agilsupriyanto14@gmail.com>
2024-10-07 20:13:52 +0700

    membuat unit test

---
+++ tests/Feature/UserServiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserServiceTest extends TestCase
+{
+     private UserService $userService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->userService = $this->app->make(UserService::class);
+    }
+
+    public function testLoginSuccess(){
+        self::assertTrue($this->userService->login("agil","rahasia"));
+    }
+
+    public function testLoginUserNotFound(){
+        self::assertFalse($this->userService->login("oke","oke"));
+    }
+
+    public function testLoginWrongPassword(){
+        self::assertFalse($this->userService->login("agil","oke"));
+
+    } 
+}
